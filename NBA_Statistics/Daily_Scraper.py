55d93cbf0c828b41d385115d16a2e43958d737cc
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-10-24 15:28:00 -0600

    Daily update files have been added, almost to full automation

--- NBA_Statistics/Daily_Scraper.py
+++ NBA_Statistics/Daily_Scraper.py
@@ -6,17 +6,36 @@ import Scraper_Master
 
 import csv
 
-print()
+print(date.today().day)
 
 def Get_Daily_Teams():
     
-    player_reference = {}
-    url = "https://www.basketball-reference.com/boxscores/index.fcgi?month=10&day=22&year=2024"
-
+    team_reference = []
+    #test url with two games... 4 teams
+    url = "https://www.basketball-reference.com/boxscores/?month=10&day=22&year=2024"
+    #real url going forward will be this:
+    #url = "https://www.basketball-reference.com/boxscores/index.fcgi?month=" + str(date.today().month) + "&day=" + str(date.today().day) + "&year=" + str(date.today().year)
+    print(url)
     team_soup = Scraper_Master.Scrape_From_Source(url)
-    #<th scope="row" class="left " data-stat="date_game" csk="202410220BOS"><a href="/boxscores/index.fcgi?month=10&amp;day=22&amp;year=2024">Tue, Oct 22, 2024</a></th>
 
+    team_rows = team_soup.find(name = "div", attrs = {"id": "content"}).findAll(name = "tr", class_=lambda x: x and any(c in x.split() for c in ["winner", "loser"]))
+    #print(team_rows)
+    for row in team_rows:
+        team = str(row.contents[1].contents[0])
+        #<a href="/teams/NYK/2025.html">New York</a>
+        team = team[16:]
+        team = team[:3]
+        #team now stores str like "NYK"
+        team_reference.append(team)
+    
+    print(team_reference)
 
+    with open('NBA_Statistics/NBA_Statistics/DailyTeamReference.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for teams in team_reference:
+            writer.writerow([teams])
+        
 
+        csvfile.close()
 
-#Get_Daily_Teams(date.today().strftime("%B").lower())
+Get_Daily_Teams()
