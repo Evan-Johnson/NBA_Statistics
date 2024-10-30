e1dbf8b39ed855b04fcc06e6550e5558c6f42aeb
Evan Johnson <johnsonevanpatrick@gmail.com>
2024-10-29 21:58:02 -0600

    October 28th games update: - only 13 successful before timeout - wondering if the problem from yesterday caused an IP ban - still wondering what that problem was from yesterday, when dailycontroller was ran it ran all the calls successfully and then ran it again for some reason completely unsuccessfully.

--- NBA_Statistics/Daily_Scraper.py
+++ NBA_Statistics/Daily_Scraper.py
@@ -15,7 +15,7 @@ def Get_Daily_Teams():
     #url = "https://www.basketball-reference.com/boxscores/?month=10&day=23&year=2024"
     #real url going forward will be this (games that happened yesterday):
     url = "https://www.basketball-reference.com/boxscores/index.fcgi?month=" + str(date.today().month) + "&day=" + str(date.today().day - 1) + "&year=" + str(date.today().year)
-    print(url)
+    
     team_soup = Scraper_Master.Scrape_From_Source(url)
 
     team_rows = team_soup.find(name = "div", attrs = {"id": "content"}).findAll(name = "tr", class_=lambda x: x and any(c in x.split() for c in ["winner", "loser"]))
@@ -28,8 +28,6 @@ def Get_Daily_Teams():
         team = team[:3]
         #team now stores str like "NYK"
         team_reference.append(team)
-    
-    print(team_reference)
 
     with open('NBA_Statistics/NBA_Statistics/DailyTeamReference.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
